edc2cc51bf634cb2436937f9bd897b4dc272d352
depeweth000 <depeweth000@warren.k12.in.us>
2021-11-17 11:47:13 -0500

    unit 3 first notes

--- L2-If Else Statements/src/Validation.java
+++ L2-If Else Statements/src/Validation.java
@@ -1,3 +1,7 @@
+import jdk.nashorn.internal.scripts.JO;
+
+import javax.swing.*;
+
 public class Validation {
 
     /*
@@ -6,6 +10,17 @@ public class Validation {
 
     Let's say a user must enter a number greater than 10.
      */
+
+    public static void main(String[] args) {
+
+        int number = Integer.parseInt(JOptionPane.showInputDialog("Please enter a number greater then ten:"));
+
+        if (number > 10){
+            JOptionPane.showMessageDialog(null, "You did it!! You may continue.");
+        }else{
+            JOptionPane.showMessageDialog(null, "That number is not greater then 10. You failed. Get a life");
+        }
+    }
 
 
 
